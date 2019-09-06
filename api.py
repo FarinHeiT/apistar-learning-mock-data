93c1b134a90344a731cd0dc93bdc2306eab2ed58
FarinHeiT <yariksuharik85@gmail.com>
2019-09-06 23:58:20 +0300

    Basic CRUD functionality

--- api.py
+++ api.py
@@ -1,11 +1,15 @@
 import json
+from typing import List
+
 from apistar import App, Route, types, validators
 from apistar.http import JSONResponse
+
+NOT_FOUND = 'Sorry! Not found...'
 
 def load_data():
 	with open('ex_prisoners.json') as f:
 		data = json.loads(f.read())
-		return {item['bracelet_ip']: item for item in data}
+		return {item['username']: item for item in data}
 
 data = load_data()
 
@@ -22,12 +26,54 @@ class Prisoner(types.Type):
 	latitude = validators.Number(maximum=90, minimum=-90)
 	longitude = validators.Number(maximum=180, minimum=-180)
 
+# API Methods
+
 def list_prisoners():
+	''' List all prisoners '''
 	return [Prisoner(prisoner[1]) for prisoner in data.items()]
+
+
+def get_prisoner(username):
+	''' Returns JSON with specified prisoner '''
+	if not data.get(username):
+		return JSONResponse({'error': NOT_FOUND}, 404)
+	return JSONResponse(data[username], 200)
+
+
+def create_prisoner(prisoner: Prisoner) -> JSONResponse:
+	''' Creates the specified prisoner ''' 
+	if data.get(prisoner.username):
+		return JSONResponse({'error': 'User already exists!'}, 400)
+
+	data[prisoner.username] = prisoner
+	return JSONResponse(Prisoner(prisoner), 200)
+
+
+def update_prisoner(username: str, prisoner: Prisoner) -> JSONResponse:
+	''' Updates the specified prisoner ''' 
+	if not data.get(username):
+		return JSONResponse({'error': NOT_FOUND}, 404)
+
+	data[prisoner.id] = prisoner
+	return JSONResponse(Prisoner(prisoner, 200))
+
+
+def remove_prisoner(username):
+	''' Deletes specified user '''
+	if not data.get(username):
+		return JSONResponse({'error': NOT_FOUND}, 404)
+
+	del data[username]
+	return JSONResponse({}, 204)
+
 
 
 routes = [
 	Route('/', method='GET', handler=list_prisoners),
+	Route('/', method='POST', handler=create_prisoner),
+	Route('/{username}/', method='GET', handler=get_prisoner),
+	Route('/{username}/', method='PUT', handler=update_prisoner),
+	Route('/{username}/', method='DELETE', handler=remove_prisoner),	
 ]
 
 app = App(routes=routes)
